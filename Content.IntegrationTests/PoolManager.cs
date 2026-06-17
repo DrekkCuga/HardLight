a6abb97574a0b31d18709e678cc190e9a8f8f718
DrekkCuga <drekk@nargacu.ga>
2026-06-17 21:46:35 +1000

    Remove Testing lines from PoolManager that I missed

--- Content.IntegrationTests/PoolManager.cs
+++ Content.IntegrationTests/PoolManager.cs
@@ -1,11 +1,7 @@
 #nullable enable
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text.Json;
 using Content.IntegrationTests.Pair;
-using Content.Shared.CCVar;
-using NUnit.Framework.Internal;
 using Robust.UnitTesting;
 
 namespace Content.IntegrationTests;
@@ -15,7 +11,6 @@ public static partial class PoolManager
 {
     public static readonly ContentPoolManager Instance = new();
     public const string TestMap = "Empty";
-    public static Dictionary<int, DateTime> pairTimers = new Dictionary<int, DateTime>();
 
     /// <summary>
     /// Runs a server, or a client until a condition is true
